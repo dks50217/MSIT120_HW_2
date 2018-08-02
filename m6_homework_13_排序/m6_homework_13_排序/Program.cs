371a5215783b8952ac7019c896549f038b5ae43e
dks50217 <dks50217@gmail.com>
2018-08-02 14:21:03 +0800

    Initial commit

---
+++ m6_homework_13_排序/m6_homework_13_排序/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        long[] v;
+        
+        v = new long[] { 9, 4, 3, 7, 2 };
+
+        Console.WriteLine("??????????????????????????");
+        for (int i = 0; i < v.Length; i++)
+        {
+            Console.Write(" " + v[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("##########################");
+        // fix #1 排序
+
+        Console.WriteLine("由小排到大");
+        Array.Sort(v);
+        for (int i = 0; i < v.Length; i++)
+        {
+            Console.Write(" " + v[i]);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("由大排到小");
+        Array.Reverse(v);
+        for (int i = 0; i < v.Length; i++)
+        {
+            Console.Write(" " + v[i]);
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+}
---
+++ m6_homework_14_隨機抽出每一個/m6_homework_13_排序/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random k = new Random();
+        double DD;
+        DD = k.NextDouble();// >= 0 && < 1 的 double
+        Console.WriteLine("DD == " + DD);
+        string[] P;
+        
+        P = new string[] { "黃建安", "科雅恆", "陳美安", "張家豪" };
+
+        Console.WriteLine("??????????????????????????");
+        for (int i = 0; i < P.Length; i++)
+        {
+            Console.Write(" " + P[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("##########################");
+        // fix #1 隨機抽出每一個
+        int Left = P.Length;
+
+        {
+            Left = P.Length;
+            for (int i = 0; i < P.Length; i++)
+            {
+                DD = k.NextDouble();
+                int index = (int)(Left * DD);
+                Console.WriteLine(index);
+                Console.Write(" " + P[index]);
+                // 對調
+                {
+                    string 記下 = P[index];
+                    P[index] = P[Left - 1];
+                    P[Left - 1] = 記下;
+                }
+
+                Left -= 1;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        {
+            Left = P.Length;
+            for (int i = 0; i < P.Length; i++)
+            {
+                DD = k.NextDouble();
+                int index = (int)(Left * DD);
+                Console.WriteLine(index);
+                Console.Write(" " + P[index]);
+                // 對調
+                {
+                    string 記下 = P[index];
+                    P[index] = P[Left - 1];
+                    P[Left - 1] = 記下;
+                }
+
+                Left -= 1;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+}
